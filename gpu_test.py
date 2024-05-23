a8c32b5f8575ffdbfecb89c63cd43e45b8989599
Matthew Frantz <mcfrantz12@gmail.com>
2024-05-23 17:24:27 -0400

    Minor changes to gpu_test

--- gpu_test.py
+++ gpu_test.py
@@ -1,5 +1,4 @@
 import torch
 
-# Check if CUDA is available and set the device
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f"Using device: {device}")
+print(torch.cuda.is_available())  # Should return True
+print(torch.cuda.get_device_name(0))  # Should return your GPU's name
